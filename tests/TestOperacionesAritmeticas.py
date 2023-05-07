b825f513cf0e7771082218bbd9f499c3c7fc02d7
“jdgamarra” <jdgamarra@uncp.edu.pe>
2023-05-07 09:54:06 -0500

    Se añadio a la prueba setup y teardown

--- tests/TestOperacionesAritmeticas.py
+++ tests/TestOperacionesAritmeticas.py
@@ -2,20 +2,24 @@ import unittest
 from src.logica.OperacionesAritmeticas import OperacionesAritmeticas
 
 class TestOperacionesAritmeticas(unittest.TestCase):
+    def setUp(self):
+        self.operacion = OperacionesAritmeticas()
+
+    def tearDown(self):
+        self.operacion = None
+
     def test_suma_dosNumeros_retornaSuma(self):
         # Arrange
-        operacion = OperacionesAritmeticas()
         sumando1 = 4
         sumando2 = 5
         resultadoEsperado = 9
 
         # Do
-        resultadoActual = operacion.suma(sumando1, sumando2)
+        resultadoActual = self.operacion.suma(sumando1, sumando2)
 
         # Assert
         self.assertEqual(resultadoEsperado,resultadoActual)
 
     def test_suma_dosNumeros_retornaSumaCorrecta(self):
-        suma = OperacionesAritmeticas()
-        self.assertEqual(4,suma.suma(2,2))
+        self.assertEqual(4,self.operacion.suma(2,2))
 
