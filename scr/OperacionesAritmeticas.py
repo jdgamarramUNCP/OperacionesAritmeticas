b69c18db18fe124f13f51f05c57739d486789aec
jdgamarra <jdgamarra@uncp.edu.pe>
2024-01-28 12:20:51 -0500

    Línea base del proyecto

---
+++ scr/OperacionesAritmeticas.py
@@ -0,0 +1,3 @@
+class OperacionesAritmeticas:
+    def suma(self, sumando1, sumando2):
+        return None
---
+++ test/PruebaSuma.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class PruebaSuma(unittest.TestCase):
+    def test_suma(self):
+        # Arrange
+
+        # Do
+
+        # Assert
+        self.assertEqual(True, False)
