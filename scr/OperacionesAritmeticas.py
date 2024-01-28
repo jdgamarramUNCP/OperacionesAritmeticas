1619fff7d6db538a7a56909819a65e2ba25690e5
jdgamarra <jdgamarra@uncp.edu.pe>
2024-01-28 13:58:15 -0500

    Se elimino TODO

--- scr/OperacionesAritmeticas.py
+++ scr/OperacionesAritmeticas.py
@@ -1,5 +1,4 @@
 class OperacionesAritmeticas:
-    # TODO Implementar la funcionalidad cuando uno de los sumandos es un flotante con coma (no permitido)
     def operacionSuma(self, sumando1, sumando2):
         for n in (sumando1, sumando2):
             if not isinstance(n, int) and not isinstance(n, float):
