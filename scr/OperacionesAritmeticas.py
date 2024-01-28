6409b68f244f7902c3dc7b55e50df99c2604f3c6
jdgamarra <jdgamarra@uncp.edu.pe>
2024-01-28 13:08:01 -0500

    ROJO: Suma de dos numeros

--- scr/OperacionesAritmeticas.py
+++ scr/OperacionesAritmeticas.py
@@ -1,3 +1,6 @@
 class OperacionesAritmeticas:
+    # TODO Implementar la funcionalidad para números enteros y flotantes
+    # TODO Implementar la funcionalidad cuando uno de los sumandos es no numérico
+    # TODO Implementar la funcionalidad cuando uno de los sumandos es un flotante con coma (no permitido)
     def suma(self, sumando1, sumando2):
         return None
--- test/PruebaSuma.py
+++ test/PruebaSuma.py
@@ -1,11 +1,16 @@
 import unittest
-
+from scr.OperacionesAritmeticas import OperacionesAritmeticas
 
 class PruebaSuma(unittest.TestCase):
-    def test_suma(self):
+    def test_suma_dosNumeros_retornaSumaNumeros(self):
         # Arrange
+        suma = OperacionesAritmeticas()
+        sumando1 = 4
+        sumando2 = 5
+        resultadoEsperado = 9
 
         # Do
+        resultadoActual = suma.suma(sumando1, sumando2)
 
         # Assert
-        self.assertEqual(True, False)
+        self.assertEqual(resultadoEsperado, resultadoActual)
