3a4a610517e9650895b8469d510dc6e9f8e47653
jdgamarra <jdgamarra@uncp.edu.pe>
2024-01-28 13:42:21 -0500

    VERDE: Un sumando no numérico

--- scr/OperacionesAritmeticas.py
+++ scr/OperacionesAritmeticas.py
@@ -3,4 +3,7 @@ class OperacionesAritmeticas:
     # TODO Implementar la funcionalidad cuando uno de los sumandos es no numérico
     # TODO Implementar la funcionalidad cuando uno de los sumandos es un flotante con coma (no permitido)
     def operacionSuma(self, sumando1, sumando2):
+        for n in (sumando1, sumando2):
+            if not isinstance(n, int) and not isinstance(n, float):
+                raise ValueError
         return sumando1 + sumando2
--- test/PruebaSuma.py
+++ test/PruebaSuma.py
@@ -38,4 +38,4 @@ class PruebaSuma(unittest.TestCase):
     def test_suma_sumandoNoNumerico_lanzaExcepcion(self):
         self.assertEqual(12, self.suma.operacionSuma(5, 7))
         with self.assertRaises(ValueError):
-            self.operacion.operacionSuma("a", 5)
+            self.suma.operacionSuma("a", 5)
